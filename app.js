42d6ccbf60d803c2914e5437df9517faf2f8505c
yasithranusha <yasithranusha24@gmail.com>
2024-03-30 04:43:35 +0530

    refactor: Organize the response data to match front end charts requirements

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const indexRoute = require("./routes/index"); // Changed this line
+const indexRoute = require("./routes/index");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
+app.use(cors()); // Use cors as middleware
 app.use(bodyParser.json());
 const morgan = require("morgan");
 
@@ -18,7 +20,7 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(morgan("dev"));
 
-app.use("/", indexRoute); // This line remains the same
+app.use("/", indexRoute);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log("Server is running on: " + PORT));
--- routes/index.js
+++ routes/index.js
@@ -38,11 +38,63 @@ router.post("/insert/:fingerNumber", (req, res, next) => {
     });
 });
 
+async function getImprovementPercentage(fingerNumber) {
+  const Model = models[`Finger${fingerNumber}`] || models.Wrist;
+  const latestDocuments = await Model.find()
+    .sort({ currentDateTime: -1 })
+    .limit(2);
+
+  if (latestDocuments.length > 0) {
+    const averages = latestDocuments.map((document) => {
+      if (document.readings && document.readings.length > 0) {
+        const readings = document.readings;
+        const total = readings.reduce(
+          (acc, curr) => acc + Number(curr.readingValue),
+          0
+        );
+        return total / readings.length;
+      } else {
+        return "No readings available";
+      }
+    });
+
+    if (typeof averages[0] === "number" && typeof averages[1] === "number") {
+      return ((averages[0] - averages[1]) / averages[1]) * 100;
+    } else {
+      return "No readings available for calculation";
+    }
+  } else {
+    return "No documents available";
+  }
+}
+
+
 router.get("/latestData/:fingerNumber", async (req, res) => {
+  const numberOfRecords = 10;
   const fingerNumber = req.params.fingerNumber; // Get the fingerNumber from the path
   const Model = models[`Finger${fingerNumber}`] || models.Wrist; // Select the correct model based on fingerNumber
-  const latestData = await Model.find().sort({ currentDateTime: -1 }).limit(5);
-  res.json(latestData);
+  const latestData = await Model.find().sort({ currentDateTime: -1 }).limit(numberOfRecords);
+
+  // Transform the data
+  let transformedData = {};
+  let count = {};
+  latestData.forEach(data => {
+    const dateTime = new Date(data.currentDateTime).toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
+    data.readings.forEach(reading => {
+      if (!transformedData[reading.readingNumber]) {
+        transformedData[reading.readingNumber] = { readingNumber: reading.readingNumber };
+        count[reading.readingNumber] = 0;
+      }
+      if (count[reading.readingNumber] < numberOfRecords) {
+        transformedData[reading.readingNumber][dateTime] = reading.readingValue;
+        count[reading.readingNumber]++;
+      }
+    });
+  });
+
+  const improvementPercentage = await getImprovementPercentage(fingerNumber);
+
+  res.json({ data: Object.values(transformedData), improvementPercentage });
 });
 
 
